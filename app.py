4bbf96af47e8d6707bc0e7ed2e9e25944b689b34
Tughan <tughanbelbek@gmail.com>
2024-11-21 14:51:02 +0300

    Update brent prices insertion to run hourly for improved data consistency

--- app.py
+++ app.py
@@ -23,7 +23,8 @@ for hour in range(24):
     scheduler.add_job(
         func=ep.insert_gas_prices, trigger=CronTrigger(hour=hour, minute=0)
     )
-scheduler.add_job(func=ep.insert_brent_prices, trigger=CronTrigger(hour=6, minute=0))
+    scheduler.add_job(func=ep.insert_brent_prices, trigger=CronTrigger(hour=hour, minute=0))
+
 scheduler.start()
 
 # Shut down the scheduler when exiting the app
