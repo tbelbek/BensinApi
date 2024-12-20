63a6a5e2cebe724924ce14264b926882e8bc4508
Tughan <tughanbelbek@gmail.com>
2024-12-20 12:09:54 +0100

    Remove unused Selenium imports from extract_prices.py

--- extract_prices.py
+++ extract_prices.py
@@ -6,12 +6,6 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-import time
 from requests_html import HTMLSession
 
 # Load environment variables from .env file
